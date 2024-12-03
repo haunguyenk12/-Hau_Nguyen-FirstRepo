37a025c6c10a2d052a9310fccdfc8d840b161546
haunguyenk12 <hau.nguyenphuc.cit22@eiu.edu.vn>
2024-12-03 13:01:04 +0700

    update input-name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello HauNguyen");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
         System.out.println(" Welcome to my repository.");
     }
 }
